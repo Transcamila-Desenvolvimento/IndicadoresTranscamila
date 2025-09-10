4abdf082f2e52503b87948417c330a06d735f48b
MiguisSsS <migueldasilvaribeiro1112@gmail.com>
2025-09-10 01:34:01 -0300

    Resolvidos conflitos com remoto

---
+++ dashboard/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from dashboard.views import login_view, logout_view, dashboard_view
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('faturamento/Cte-FaturadosXafaturar/', views.cte_pendentes_view, name='cte'),
+    path('faturamento/ctesfaturadosxafaturar/', views.cte_pendentes_view, name='cte'),
+    path('faturamento/ctesrecebidos/', views.ctesrecebidos_view, name='cterecebidos'),
+    path('faturamento/clientescominadimplência/', views.inadimplencia, name='inadimplencia'),
+    path('financeiro/Statusdeops/', views.op, name='op'),
+    path('financeiro/ICMSporConformidade/', views.icms, name='icms'),
+    path('financeiro/pagamentossemlanlcamento/', views.nf, name='nf'),
+    path('recursoshumanos/distribuicaodaforcadetrabalho/', views.força, name='força'),
+    path('recursoshumanos/jornada/', views.jornada, name='jornada'),
+    path('recursoshumanos/recrutamentoeselecao/', views.recrutamento, name='recrutamento'),
+    path('provisao/conferencia/', views.conferencia, name='conferencia'),
+    path('provisao/lancamentoaeepc/', views.lancamento, name='lancamento'),
+]
